51c70700e3bccb4e4768ca92db0cf9dbdb44f8b1
mlinkus <matthew.linkus@gmail.com>
2013-10-11 15:21:42 -0400

    Comment correction

--- 073_Solution.cpp
+++ 073_Solution.cpp
@@ -16,7 +16,7 @@ int main()
 
 	int fractions = 0;
 	
-	// For each denominator d = 1, 2, ..., 1000000
+	// For each denominator d = 1, 2, ..., 12000
 	for(int n=1, d=2; d <= 12000;d++)
 	{
 		// Get a numerator to make n/d just above 1/3, proceed until n/d >= 1/2
