7b18c49f8af6cfd9ba61d50ce18b893d017a98a7
mlinkus <matthew.linkus@gmail.com>
2013-10-11 13:31:11 -0400

    Solved #073

---
+++ 073_Solution.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+int gcd(int a, int b)
+{
+	if (b == 0)
+		return a;
+	else
+		return gcd(b, a % b);
+}
+
+int main()
+{
+	std::cout << " -----------\n"
+		<< "Project Euler Problem #073 Solution\n"
+		<< " -----------\n\n";
+
+	int fractions = 0;
+	
+	// For each denominator d = 1, 2, ..., 1000000
+	for(int n=1, d=2; d <= 12000;d++)
+	{
+		// Get a numerator to make n/d just above 1/3, proceed until n/d >= 1/2
+		for(n = (d*1/3 + 1); n*2 < d; n++)
+		{
+			if (gcd(d, n) == 1)
+				fractions++;
+		}
+	}
+	
+	std::cout << "Total fractions between 1/3 and 1/2: " << fractions;
+
+	return 0;
+ }
