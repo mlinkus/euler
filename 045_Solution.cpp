021b49416b806bf84507d729379a7f881438c5e1
mlinkus <matthew.linkus@gmail.com>
2013-10-23 16:35:32 -0400

    Code #045 Improved

--- 045_Solution.cpp
+++ 045_Solution.cpp
@@ -1,15 +1,12 @@
 #include <iostream>
 #include <cmath>
-using namespace std;
 
 int is_tri(unsigned __int64 x)
 {
-	float val = 0;
-	val = ((pow(1+8*x, .5) - 1) / 2);
-	long int val2 = val;
-	if (val == val2)
+	double val = ((pow(1+8*x, .5) - 1) / 2);
+	if (val == floor(val))
 	{
-		cout << x << " is triangular" << endl;
+		std::cout << x << " is triangular\n";
 		return 1;
 	}
 	else return 0;
@@ -17,11 +14,10 @@ int is_tri(unsigned __int64 x)
 
 int is_pent(unsigned __int64 x)
 {
-	double val = 0;
-	val = (pow(1+24*x, .5) + 1) / 6;
+	double val = (pow(1+24*x, .5) + 1) / 6;
 	if (val == floor(val))
 	{
-		cout << x << " is pentagonal" << endl;
+		std::cout << x << " is pentagonal\n";
 		return 1;
 	}
 	else return 0;
@@ -29,30 +25,30 @@ int is_pent(unsigned __int64 x)
 
 int main()
 {
+	std::cout << " -----------\n"
+		<< "Project Euler Problem #045 Solution\n"
+		<< " -----------\n\n";
+
+	// Make an iterator for the nth hexagonal number and somewhere to store it.
 	int n;
 	unsigned __int64 hexnum;
-	bool done = 0;
 
-	cout << " -----------" << endl;
-	cout << "Project Euler Problem #045 Solution" << endl;
-	cout << " -----------" << endl << endl;
+	// Input a starting n, use this to test that 40755 (n = 143) meets the criteria.
+	std::cout << "Start at n = ?  ";
+	std::cin >> n;
 	
-	cout << "Start at n = ?" << endl;
-	cin >> n;
-
-	// We go through each hexagonal number and test if it is pentagonal and triangular.
-	while(!done)
-	{
+	// Until we find a number that is both pentagonal and hexagonal...
+	do {
+		// Make each hexagonal number and output it.
 		hexnum = n * (2*n - 1);
-		cout << "Trying " << hexnum << endl;
-		// if (is_tri(hexnum) && is_pent(hexnum)) done = 1;
-		// Note: Every hexagonal number is triangular, we only need check if it is pentagonal.
-		// We'll just check if it's pentagonal FIRST and verify triangularity for posterity.
-		if (is_pent(hexnum) && is_tri(hexnum)) done = 1;
-		else n++;
-	}
+		std::cout << "Trying " << hexnum << "\n";
+		n++;
+	} while (!(is_pent(hexnum) && is_tri(hexnum)));
+	
+	// Note: Every hexagonal number is also triangular, we only need check if it is pentagonal.
+	// We'll just check if it's pentagonal FIRST and verify triangularity for posterity.
 
-	cout << hexnum << endl;
+	std::cout << hexnum << "\n";
 
 	return 0;
 }
